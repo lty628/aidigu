90ee05ab477c104c7a2453302897128851a6cd72
SR.李 <lifulong@orangevip.net>
2023-10-30 16:33:53 +0800

    隐身bug调整

--- application/common/controller/IndexInfo.php
+++ application/common/controller/IndexInfo.php
@@ -33,6 +33,7 @@ class IndexInfo extends Info
             ->field('user.uid,user.nickname,user.head_image,user.blog,message.ctime,message.contents,message.repost,message.refrom,message.repostsum,message.image,message.image_info,message.commentsum,message.msg_id')
             ->where('message.is_delete', 0)
             ->where('user.invisible', 0)
+            ->where('user.uid', $this->siteUserId)
             ->paginate(30, false, ['page' => request()->param('page/d', 1), 'path' => '[PAGE].html']);
         }
         
