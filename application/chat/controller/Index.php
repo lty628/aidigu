792cc5eec02276fb423fc2771e997bf71b813b86
SR.李 <lifulong@orangevip.net>
2023-12-11 13:46:57 +0800

    私信功能调整

--- application/chat/controller/Index.php
+++ application/chat/controller/Index.php
@@ -1,6 +1,7 @@
 <?php
 namespace app\chat\controller;
 use think\Controller;
+use app\chat\model\ChatPrivateLetter;
 
 class Index extends Controller
 {
@@ -9,10 +10,28 @@ class Index extends Controller
         $touid = input('private');
         $fromuid = getLoginUid();
         $wsserver = env('app.chatSocketDomain');
+
+        if ($touid || $fromuid) {
+            $this->checkPrivate($touid, $fromuid);
+        }
+
         // $domain = 'https://'.$urlDomainRoot;
         $this->assign('wsserver', $wsserver);
         $this->assign('touid', $touid);
         $this->assign('fromuid', $fromuid);
         return $this->fetch();
+    }
+
+    protected function checkPrivate($touid, $fromuid)
+    {
+        $model = new ChatPrivateLetter();
+        $info = $model->where([
+            'fromuid' => $fromuid,
+            'touid' => $touid
+        ])->find();
+        if (!$info) {
+            return $model->addPrivateFriend($touid, $fromuid);
+        }
+        return true;
     }
 }
