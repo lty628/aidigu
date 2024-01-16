ce50d499c153d7bb70771abf07a1ae15937c6b21
SR.李 <lifulong@orangevip.net>
2024-01-16 15:45:48 +0800

    修改缓存提醒

--- application/common/libs/Remind.php
+++ application/common/libs/Remind.php
@@ -28,8 +28,12 @@ class Remind
         return cache(self::getName($uid), $data);
     }
 
-    public static function clean($uid)
+    public static function clean($uid, $appNameKey = 'chat')
     {
-        return cache(self::getName($uid), []);
+        $data = self::check($uid);
+        if (in_array($appNameKey, $data)) {
+            $data = array_values(array_diff($data, [$appNameKey]));
+        }
+        return cache(self::getName($uid), $data);
     }
 }
