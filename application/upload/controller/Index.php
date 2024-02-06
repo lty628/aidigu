5a345da812b220f3d3dcf7e15cd278e5df9f1e5f
SR.李 <lifulong@orangevip.net>
2024-02-06 16:28:13 +0800

    网盘新增搜索

--- application/upload/controller/Index.php
+++ application/upload/controller/Index.php
@@ -33,15 +33,33 @@ class Index extends Controller
 
     public function getFiles()
     {
-        $result = Db::name('file')->where('is_delete', 0)->order('create_time', 'desc')->where('userid', getLoginUid())->select();
-        return json(['code' => 0, 'data'=> $result]);
+        $get = input('get.');
+        $page = $get['page'] ?? 1;
+        $limit = $get['limit'] ?? 10;
+        $fileName = $get['file_name'] ?? '';
+        $where[] = ['userid', '=', getLoginUid()];
+        if ($fileName) {
+            $where[] = ['file_name', 'like',  '%'.$fileName.'%'];
+        }
+        $count = Db::name('file')->where('is_delete', 0)->where($where)->count();
+        $result = Db::name('file')->where('is_delete', 0)->order('create_time', 'desc')->where($where)->limit($limit)->page($page)->select();
+        return json(['code' => 0, 'data'=> $result, 'count' => $count]);
     }
 
     // 回收站
     public function getCollection()
     {
-        $result = Db::name('file')->where('is_delete', 1)->order('create_time', 'desc')->where('userid', getLoginUid())->select();
-        return json(['code' => 0, 'data'=> $result]);
+        $get = input('get.');
+        $page = $get['page'] ?? 1;
+        $limit = $get['limit'] ?? 10;
+        $fileName = $get['file_name'] ?? '';
+        $where[] = ['userid', '=', getLoginUid()];
+        if ($fileName) {
+            $where[] = ['file_name', 'like',  '%'.$fileName.'%'];
+        }
+        $count = Db::name('file')->where('is_delete', 1)->where($where)->count();
+        $result = Db::name('file')->where('is_delete', 1)->order('create_time', 'desc')->where($where)->limit($limit)->page($page)->select();
+        return json(['code' => 0, 'data'=> $result, 'count' => $count]);
     }
 
     // 回收站
