da312b11c9139470476cba9b9ee0ec94985fafdf
SR.李 <lifulong@orangevip.net>
2024-02-22 13:48:39 +0800

    topic新增创建人

--- update.sql
+++ update.sql
@@ -4,5 +4,5 @@ ADD COLUMN `invite_code` varchar(255) NOT NULL DEFAULT '' AFTER `fromuid`;
 ADD COLUMN `invite_status` tinyint UNSIGNED NOT NULL DEFAULT 0 COMMENT '是否开启加群' AFTER `invite_code`;
 ADD COLUMN `utime` bigint UNSIGNED NULL AFTER `ctime`;
 
-ALTER TABLE `aidigu_cn`.`wb_topic` 
+ALTER TABLE `wb_topic` 
 ADD COLUMN `uid` bigint(0) UNSIGNED NOT NULL DEFAULT 0 COMMENT '创建人，默认0' AFTER `title`;
