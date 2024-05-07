f592ff1dbfbfa6f944e43335e06dec0879c8947c
SR.李 <lifulong@orangevip.net>
2024-05-07 20:35:52 +0800

    无

--- public/static/chat/js/edit.js
+++ public/static/chat/js/edit.js
@@ -324,80 +324,80 @@ const E = window.wangEditor
 		}
 	}
 
-	class AlertMaterial extends BtnMenu {
-		constructor(editor) {
-			// data-title属性表示当鼠标悬停在该按钮上时提示该按钮的功能简述
-			const $elem = E.$(
-				`<div class="w-e-menu" data-title="素材">
-					<i class="layui-icon layui-icon-website"></i> 
-				</div>`
-			)
-			super($elem, editor)
-		}
-		// 菜单点击事件
-		clickHandler() {
-			// 做任何你想做的事情
-			// 可参考【常用 API】文档，来操作编辑器
-			var area = ['70%', '80%']
-			parent.layer.open({
-				type: 2,
-				title: '选择素材',
-				shade: 0.6,
-				area: area,
-				resize: false,
-				maxmin: false,
-				closeBtn: true,
-				shadeClose: true,
-				content: '/tools/sourcematerial/share',
-				zIndex: layer.zIndex, //重点1
-				// success: function (layero) {
-				// 	layer.setTop(layero); //重点2
-				// },
-				end: function () {
-					var sourcematerial = layui.sessionData('sourcematerial')
-					if (!sourcematerial.data) {
-						return false
-					}
+	// class AlertMaterial extends BtnMenu {
+	// 	constructor(editor) {
+	// 		// data-title属性表示当鼠标悬停在该按钮上时提示该按钮的功能简述
+	// 		const $elem = E.$(
+	// 			`<div class="w-e-menu" data-title="素材">
+	// 				<i class="layui-icon layui-icon-website"></i> 
+	// 			</div>`
+	// 		)
+	// 		super($elem, editor)
+	// 	}
+	// 	// 菜单点击事件
+	// 	clickHandler() {
+	// 		// 做任何你想做的事情
+	// 		// 可参考【常用 API】文档，来操作编辑器
+	// 		var area = ['70%', '80%']
+	// 		parent.layer.open({
+	// 			type: 2,
+	// 			title: '选择素材',
+	// 			shade: 0.6,
+	// 			area: area,
+	// 			resize: false,
+	// 			maxmin: false,
+	// 			closeBtn: true,
+	// 			shadeClose: true,
+	// 			content: '/tools/sourcematerial/share',
+	// 			zIndex: layer.zIndex, //重点1
+	// 			// success: function (layero) {
+	// 			// 	layer.setTop(layero); //重点2
+	// 			// },
+	// 			end: function () {
+	// 				var sourcematerial = layui.sessionData('sourcematerial')
+	// 				if (!sourcematerial.data) {
+	// 					return false
+	// 				}
 	
-					var data = sourcematerial.data
+	// 				var data = sourcematerial.data
 	
-					$(".tool-up-class").show();
-					$("#mediaVal").val(JSON.stringify(data));
-					if (data.media_title) {
-						var topicTitle = $("#topicTitle").val()
-						if (topicTitle) {
-							editor.txt.html(topicTitle+"&nbsp;"+data.media_title)
-						} else {
-							editor.txt.html(data.media_title)
-						}
-					}
+	// 				// $(".tool-up-class").show();
+	// 				$("#mediaVal").val(JSON.stringify(data));
+	// 				if (data.media_title) {
+	// 					var topicTitle = $("#topicTitle").val()
+	// 					if (topicTitle) {
+	// 						editor.txt.html(topicTitle+"&nbsp;"+data.media_title)
+	// 					} else {
+	// 						editor.txt.html(data.media_title)
+	// 					}
+	// 				}
 					
-					layui.sessionData('sourcematerial', null)
-				}
-			});
-		}
-		// 菜单是否被激活（如果不需要，这个函数可以空着）
-		// 1. 激活是什么？光标放在一段加粗、下划线的文本时，菜单栏里的 B 和 U 被激活，如下图
-		// 2. 什么时候执行这个函数？每次编辑器区域的选区变化（如鼠标操作、键盘操作等），都会触发各个菜单的 tryChangeActive 函数，重新计算菜单的激活状态
-		tryChangeActive() {
-			// 激活菜单
-			// 1. 菜单 DOM 节点会增加一个 .w-e-active 的 css class
-			// 2. this.this.isActive === true
-			// this.active()
+	// 				layui.sessionData('sourcematerial', null)
+	// 			}
+	// 		});
+	// 	}
+	// 	// 菜单是否被激活（如果不需要，这个函数可以空着）
+	// 	// 1. 激活是什么？光标放在一段加粗、下划线的文本时，菜单栏里的 B 和 U 被激活，如下图
+	// 	// 2. 什么时候执行这个函数？每次编辑器区域的选区变化（如鼠标操作、键盘操作等），都会触发各个菜单的 tryChangeActive 函数，重新计算菜单的激活状态
+	// 	tryChangeActive() {
+	// 		// 激活菜单
+	// 		// 1. 菜单 DOM 节点会增加一个 .w-e-active 的 css class
+	// 		// 2. this.this.isActive === true
+	// 		// this.active()
 	
-			// // 取消激活菜单
-			// // 1. 菜单 DOM 节点会删掉 .w-e-active
-			// // 2. this.this.isActive === false
-			// this.unActive()
-		}
-	}
+	// 		// // 取消激活菜单
+	// 		// // 1. 菜单 DOM 节点会删掉 .w-e-active
+	// 		// // 2. this.this.isActive === false
+	// 		// this.unActive()
+	// 	}
+	// }
 
 	const menuKey1 = 'AlertCloud' 
 	editor.menus.extend(menuKey1, AlertCloud)
 	editor.config.menus = editor.config.menus.concat(menuKey1)
-	const menuKey2 = 'AlertMaterial' 
-	editor.menus.extend(menuKey2, AlertMaterial)
-	editor.config.menus = editor.config.menus.concat(menuKey2)
+	// const menuKey2 = 'AlertMaterial' 
+	// editor.menus.extend(menuKey2, AlertMaterial)
+	// editor.config.menus = editor.config.menus.concat(menuKey2)
 
 
 	editor.create()
