e91427202b30a5a7d7f4d9dabe2ced674f88d500
SR.李 <352897321@qq.com>
2025-02-07 22:22:19 +0800

    优化小说阅读

---
+++ application/tools/controller/Reader.php
@@ -0,0 +1,46 @@
+<?php
+namespace app\tools\controller;
+use think\Controller;
+use think\Db;
+
+
+class Reader extends Controller
+{	
+	public function index()
+    {
+        $materialRelationId = (int) input('get.material_relation_id');
+        $path = '';
+        $contents = '';
+        $filename = '';
+
+        if ($materialRelationId) {
+            $materialRelation = Db::name('source_material_relation')->field('id,media_info,media_type,file_name')->where('id', $materialRelationId)->find();
+            if ($materialRelation) {
+                $path = $materialRelation['media_info'] . '.' . $materialRelation['media_type'];
+                $filename = $materialRelation['file_name'];
+            }
+        }
+        if ($path) {
+            $path = env('root_path') . 'public' . $path;
+        }
+
+        if (file_exists($path)) {
+            $contents = file_get_contents($path);
+            // 判断字符集并改为utf8
+            $charset = mb_detect_encoding($contents, array("ASCII", "GB2312", "GBK", "UTF-8"));
+            if ($charset != 'UTF-8') {
+                $contents = mb_convert_encoding($contents, 'UTF-8', $charset);
+            }
+
+        }
+
+        if ($path) {
+            $path = 1;
+        }
+        
+        $this->assign('path',$path);
+        $this->assign('contents',$contents);
+        $this->assign('filename',$filename);
+        return $this->fetch();
+    }
+}
