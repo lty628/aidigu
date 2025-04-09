fa3983f730d2d4076a603b13394800894d72abf6
SR.李 <352897321@qq.com>
2025-04-10 00:00:20 +0800

    无

--- application/tools/controller/Media.php
+++ application/tools/controller/Media.php
@@ -18,49 +18,60 @@ class Media extends Controller
     {
         return $this->fetch();
     }
-    
-    // 新增获取日记列表方法
+
+    // 修改获取日记列表方法，不再分页，获取指定日期的所有数据
     public function getDiaryList()
     {
         $userid = getLoginUid();
         $date = input('param.date', '');
-    
-        // 如果没有提供日期，默认查询当前月的1号
+
+        // 如果没有提供日期，默认查询当前日期
         if (!$date) {
-            $date = date('Y-m-01');
+            $date = date('Y-m-d');
         }
-    
+
         $prefix = config('database.prefix');
         $query = Db::name('message')
             ->alias('message')
             ->where('message.uid', $userid) // 只查询自己的日记
             ->where('message.is_delete', 0);
-    
+
         // 添加日期筛选条件
         $dateStart = strtotime($date . ' 00:00:00');
-        $dateEnd = time();
+        $dateEnd = strtotime($date . ' 23:59:59');
         $query->where('message.ctime', 'between', [$dateStart, $dateEnd]);
-    
+
         $userMessage = $query
             ->order('message.ctime asc')
-            ->field('uid,is_delete,commentsum,repostsum,collectsum',true)
-            // ->field('user.uid,user.nickname,user.head_image,user.blog,message.ctime,message.contents,message.repost,message.refrom,message.repostsum,message.media,message.media_info,message.commentsum,message.msg_id')
-            ->paginate(8, false, ['page' => request()->param('page/d', 1), 'path' => '[PAGE].html']);
-        
-        $userMessage = $userMessage->toArray();
-        $userMessage['data'] = handleMessage($userMessage['data']);
-        $userMessage['allow_delete'] = 1;
+            ->field('uid,is_delete,commentsum,repostsum,collectsum', true)
+            ->select();
+
         // dump($userMessage);die;
-        return json(array('status' =>  1, 'msg' => 'ok', 'data' => $userMessage));
+        return json(array('status' =>  1, 'msg' => 'ok', 'data' => [
+            'data' => handleMessage($userMessage),
+            'allow_delete' => 1,
+            'date' => $date, // 返回查询的日期
+        ]));
     }
+
     public function getDiaryDates() {
         $uid = getLoginUid();
         $month = input('param.month', date('Y-m')); // 默认当前月
-        
+
+        // 验证 month 参数格式
+        if (!preg_match('/^\d{4}-\d{2}$/', $month)) {
+            return json([
+                'status' => 0, 
+                'msg' => '参数格式错误，month 应为 Y-m 格式', 
+                'data' => [],
+                'current_month' => null
+            ]);
+        }
+
         // 计算月份的开始和结束时间
         $startTime = strtotime($month . '-01 00:00:00');
         $endTime = strtotime(date('Y-m-t 23:59:59', $startTime));
-        
+
         $dates = Db::name('message')
             ->where('uid', $uid)
             ->where('ctime', 'between', [$startTime, $endTime])
@@ -68,7 +79,7 @@ class Media extends Controller
             ->group("FROM_UNIXTIME(ctime, '%Y-%m-%d')")
             ->order('date desc')
             ->select();
-        
+
         return json([
             'status' => 1, 
             'msg' => 'ok', 
