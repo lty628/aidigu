f551457c788450a296dad1117963cee09ca4a008
SR.李 <352897321@qq.com>
2025-12-04 15:07:06 +0800

    新增系统配置

--- application/upload/controller/Upload.php
+++ application/upload/controller/Upload.php
@@ -8,7 +8,8 @@ class Upload extends Controller
 {
     public function __construct()
     {
-        \app\upload\libs\Config::set(env('CONFIG_PATH').'upload.php');
+        // \app\upload\libs\Config::set(env('CONFIG_PATH') . 'upload.php');
+        \app\upload\libs\Config::set($this->getConfig());
     }
     public function index()
     {
@@ -22,4 +23,47 @@ class Upload extends Controller
         exit(0); // Exit from current PHP process.
     }
 
+    protected function getConfig()
+    {
+        return [
+
+            'cache' => [
+                'redis' => [
+                    'host' => getenv('REDIS_HOST') !== false ? getenv('REDIS_HOST') : '127.0.0.1',
+                    'port' => getenv('REDIS_PORT') !== false ? getenv('REDIS_PORT') : '6379',
+                    'database' => getenv('REDIS_DB') !== false ? getenv('REDIS_DB') : 0,
+                ],
+
+                /**
+                 * File cache configs.
+                 */
+                'file' => [
+                    'dir' => env('ROOT_PATH') . '.cache' . DIRECTORY_SEPARATOR,
+                    'name' => 'php.server.cache',
+                ],
+            ],
+            'storge' => [
+                // 值为类名
+                'type' => sysConfig('storage.type'), // File S3File ...
+                'FileConfig' => [
+                    'Path' => '/uploads/cloud_upload/'
+                ],
+                // 在S3File中使用的配置
+                'S3FileConfig' => [
+                    'credentials' => [
+                        'key'    => sysConfig('s3Config.awsAccessKey'),
+                        'secret' => sysConfig('s3Config.awsSecretKey'),
+                    ],
+                    'Bucket' => sysConfig('s3Config.bucket'),
+                    'region' => sysConfig('s3Config.region'),
+                    'version' => 'latest',
+                    'endpoint' => sysConfig('s3Config.endpoint'),
+                    'ACL'    => 'public-read',
+                    'use_path_style_endpoint' => true,
+                    // 保存路径
+                    'Path' => '',
+                ],
+            ],
+        ];
+    }
 }
