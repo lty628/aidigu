f6ab0dcbf7ef63947a2a37b47ccd77b6ceb46320
SR.李 <352897321@qq.com>
2026-02-04 08:30:35 +0800

    无

--- application/tools/controller/Home.php
+++ application/tools/controller/Home.php
@@ -15,8 +15,8 @@ class Home extends Controller
         $this->assign('beian', sysConfig('app.beian', ''));
         $this->assign('userInfo', $userInfo);
         // 近期6篇文章
-        $list = Db::name('cms_content')->order('content_id desc')->limit(6)->select();
-        $this->assign('articleList', $list);
+        // $list = Db::name('cms_content')->order('content_id desc')->limit(6)->select();
+        // $this->assign('articleList', $list);
         return $this->fetch();
     }
 }
