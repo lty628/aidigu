332c3ed3cda5691d3b0fb5e357156c7426b927cf
SR.李 <352897321@qq.com>
2026-02-05 09:41:55 +0800

    无

--- application/tools/controller/Home.php
+++ application/tools/controller/Home.php
@@ -7,11 +7,11 @@ class Home extends Controller
 {	
 	public function index()
     {
-        $userInfo = getLoginUserInfo();
-        // 自动登录
-        if (!$userInfo && cookie('rememberMe')) {
-            $userInfo = checkUserCookie(cookie('rememberMe'));
-        }
+        // $userInfo = getLoginUserInfo();
+        // // 自动登录
+        // if (!$userInfo && cookie('rememberMe')) {
+        //     $userInfo = checkUserCookie(cookie('rememberMe'));
+        // }
 
         $host = request()->host();
         $scheme = request()->scheme();
@@ -23,7 +23,7 @@ class Home extends Controller
         $this->assign('host', $host);
         $this->assign('scheme', $scheme);
         $this->assign('beian', sysConfig('app.beian', ''));
-        $this->assign('userInfo', $userInfo);
+        // $this->assign('userInfo', $userInfo);
         // 近期6篇文章
         // $list = Db::name('cms_content')->order('content_id desc')->limit(6)->select();
         // $this->assign('articleList', $list);
